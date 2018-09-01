d4ae756dc278b0c4b5b4812d87cd223f6e7013ca
shirleyzong <szong1@babson.edu>
2018-08-31 23:59:58 -0400

    ex1

---
+++ session 1/calc.py
@@ -0,0 +1,15 @@
+#question 1:2562s
+print(42*60+42)
+#question 2:6.21miles
+print(10/1.61)
+#question 3:
+first=42*60+4
+second=10/1.61
+#time per mile in seconds:406.364
+print(first/second)
+#time per mile in minutes:6.772
+third=first/second
+print(third/60)
+#average speed in miles per hour: 8.859
+fourth=first/60/60
+print(second/fourth)
